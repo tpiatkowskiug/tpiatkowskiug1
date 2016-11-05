a3c96a01c96c983f9eee65e08e42bbc4c7539105
Tomasz Piątkowski <tpiatkowski@p29-14.labpk.inf.ug.edu.pl>
2016-11-05 13:00:26 +0100

    zadanie3

--- zadanie3.c
+++ zadanie3.c
@@ -4,39 +4,37 @@ int main () {
 int tab [7];
 int max=-99999;
 int min=99999;
-int
+int suma=0;
+double srednia;
 for (int i = 0; i < 7; i=i+1) {
   scanf("%d", &tab[i]);
 }
 
-  /liczba największa/
-  for (int i = 0; i < 7; i=i+1);
-  if (tab[i]>max)max=tab[i];{
-  printf("max = %d",max );
-  printf("\n", max);
+  //liczba największa/
+  for (int i = 0; i < 7; i=i+1) {
+  if (tab[i]>max){max=tab[i];
 }
-  /liczby najmnijesza/
-  for (int i = 0; i < 7; i=i+1);
-  if (tab[i]<min)min=tab[i];{
-  printf("min = %d",min );
-  printf("\n" min)
 }
-  /średnia arytmetyczna elemetów tablicy
-  for (int i = 0; i < 7; i=i+1);
+printf("max = %d",max );
+  //liczby najmnijesza/
+  for (int i = 0; i < 7; i=i+1) {
+  if (tab[i]<min) {min=tab[i];
+
+}
+}
+printf("min = %d",min );
+  //średnia arytmetyczna elemetów tablicy
+  for (int i = 0; i < 7; i=i+1){
   suma = suma + tab [i];
-  średnia = (float) suma/ i < 7; {
-  printf ("Suma: %d, średnia: %f\n\n");
+  srednia = (double) suma/ i;
+
 }
-  /ilości liczby większej od stu/
-  for (int i = 0; i < 7; i=i+1);
+  printf ("Suma: %d, średnia: %f\n\n" ,suma, srednia);
+ //ilości liczby większej od stu/
+  for (int i = 0; i < 7; i=i+1) {
   if (tab[i]<100){
-  printf("%d\n", );
+  //printf("%d\n", );
 }
-
-
-
-
-
-
+}
 return 0;
 }
