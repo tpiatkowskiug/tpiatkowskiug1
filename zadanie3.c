d092329f3d10f293ad674f66f35dacef064c722d
Tomek Piątkowski <tpiatkowski@sigma.ug.edu.pl>
2016-11-15 01:25:22 +0100

    większe od stu

--- zadanie3.c
+++ zadanie3.c
@@ -6,20 +6,20 @@ int max=-99999;
 int min=99999;
 int suma=0;
 double srednia;
+int nLiczba=0;
 for (int i = 0; i < 7; i=i+1) {
   scanf("%d", &tab[i]);
 }
 
-  //liczba największa/
+  //liczba największa
   for (int i = 0; i < 7; i=i+1) {
   if (tab[i]>max){max=tab[i];
 }
 }
 printf("max = %d\n", max );
-  //liczby najmnijesza/
+  //liczby najmnijesza
   for (int i = 0; i < 7; i=i+1) {
   if (tab[i]<min) {min=tab[i];
-
 }
 }
 printf("min = %d\n",min );
@@ -27,14 +27,15 @@ printf("min = %d\n",min );
   for (int i = 0; i < 7; i=i+1){
   suma = suma + tab [i];
   srednia = (double) suma/ i;
-
 }
   printf ("suma: %d\nsrednia: %f\n" ,suma, srednia);
- //ilości liczby większej od stu/
+ //ilości liczb większej od stu
+ printf ("\nilości liczb większej od stu\n");
   for (int i = 0; i < 7; i=i+1) {
-  if (tab[i]<100){
-  //printf("%d\n", );
+  tab[i] >100;
+  nLiczba = tab [i];
 }
-}
+printf ("nLiczba= %d\n", nLiczba);
+
 return 0;
 }
