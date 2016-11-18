b5c1b0a94275295118acf1154b61f45704c1291f
Magdalena Sarzyńska <magda2609@users.noreply.github.com>
2016-11-18 22:20:06 +0100

    Update zadanie3.c
    a) 2.5/2.5
    b) 2.5/2.5
    c) 2.5/2.5
    d) 2.5/2.5
    
    Razem 10 pkt na 10 możliwych pkt.

--- zadanie3.c
+++ zadanie3.c
@@ -2,7 +2,7 @@
 
 int main () {
 int tab [7];
-int max=-99999;
+int max=-99999; //lepiej byłoby wstawić: max = tab[0]; min = tab[0];
 int min=99999;
 int suma=0;
 int ilosc=0;
@@ -26,7 +26,7 @@ printf("min = %d\n",min );
   //średnia arytmetyczna elemetów tablicy
   for (int i = 0; i < 7; i=i+1){
   suma = suma + tab [i];
-  srednia = (double) suma/ i;
+  srednia = (double) suma/ i;   //srednia moglaby byc liczona po wyjsciu z petli for. Operacja powinna byc wykonana tylko raz, nie 7.
 }
   printf ("suma: %d\nsrednia: %f\n" ,suma, srednia);
  //ilości liczb większej od stu
@@ -39,3 +39,10 @@ printf("min = %d\n",min );
 printf ("ilosc = %d\n", ilosc);
 return 0;
 }
+/*
+a) 2.5/2.5
+b) 2.5/2.5
+c) 2.5/2.5
+d) 2.5/2.5
+
+Razem 10 pkt na 10 możliwych pkt.*/
